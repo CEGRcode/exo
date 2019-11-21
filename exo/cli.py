ad37a9b1532fa6dba852b9c146c4dbae3a3b9e79
PrashantKuntala <prashantkuntala@gmail.com>
2019-11-21 15:49:20 -0500

    Add calculate-contrast utility, version bump to 0.1.1

--- exo/cli.py
+++ exo/cli.py
@@ -3,11 +3,14 @@
 """Console script for exo."""
 import sys
 import click
+from exo import __author__
+from exo import __version__
 from pyfiglet import Figlet
 
-
-@click.command()
-def main():
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(options_metavar='<options>', context_settings=CONTEXT_SETTINGS)
+@click.option('-v','--version', is_flag=True, help="print current version")
+def main(version):
     """
     exo is a collection of command-line utilities for Yeast Epigenome Project
 
@@ -18,12 +21,17 @@ def main():
         make-heatmap        : generates a heatmap from a tagPileUP CDT tabular file.
         make-composite      : generates a composite from a tagPileUP CDT tabular file.
 
-    For help with each utility, use --help (ex: make-heatmap --help)
+    For help with each utility, use -help (ex: make-heatmap -help)
     """
-    f = Figlet(font='larry3d')
-    click.echo(f.renderText('yeast epigenome project !'))
-    click.echo("Usage: exo --help \n")
+    if version:
+        click.echo(__version__)
+    else:
+        f = Figlet(font='larry3d')
+        click.echo(f.renderText('yeast epigenome project !'))
+        click.echo("Author: "+ __author__+ "\nVersion: "+__version__)
+        click.echo("Usage: exo --help \n")
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- setup.py
+++ setup.py
@@ -8,27 +8,25 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ['Click>=7.0','matplotlib','numpy', 'pyfiglet' ]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
+requirements = ['Click>=7.0', 'matplotlib', 'numpy', 'pyfiglet']
 
 setup(
     author="Prashant Kumar Kuntala",
     author_email='prashantkuntala@gmail.com',
-    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.7',
     ],
     description="Quality Control and Visualization utilities for Yeast Epigenome Project ",
     entry_points={
         'console_scripts': [
             'exo=exo.cli:main',
+            'calculate-contrast=exo.calculate_contrast_threshold:cli',
         ],
     },
     install_requires=requirements,
@@ -37,7 +35,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords='exo',
     name='exo',
-    packages=find_packages(include=['exo',]),
+    packages=find_packages(include=['exo', ]),
     url='https://github.com/CEGRcode/exo',
-    version='0.1.0',
+    version='0.1.1',
 )
